cd561e7c1485132b1e33133d50f89a045597da74
andy489 <stoev.andy@gmail.com>
2026-09-04 22:07:26 +0300

    refactor: move OpenWeatherMap API key out of source into env
    Key was hardcoded in weather.js; now read from OPENWEATHERMAP_API_KEY
    env var via Spring, injected into the page as window.WEATHER_API_KEY.

--- src/main/java/com/casino/blackjack/controller/HomeController.java
+++ src/main/java/com/casino/blackjack/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.casino.blackjack.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController extends BaseController {
 
+    @Value("${openweathermap.api-key:}")
+    private String weatherApiKey;
 
     @GetMapping({"/", "/index"})
     public ModelAndView getIndex() {
-
-        return super.view("index");
+        ModelAndView mv = super.view("index");
+        mv.addObject("weatherApiKey", weatherApiKey);
+        return mv;
     }
 
     @GetMapping("/rules")
